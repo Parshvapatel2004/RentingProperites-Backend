7a426ff3b7653bb5f7a03c3e9a8ad6f6a1445c38
Parshva <codewithpasshu@gmail.com>
2025-03-15 17:23:40 +0530

    Login Api created

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 const dbConnect = require('./DB/db.Connect');
+const express = require("express");
+const cors = require("cors");
+const { Login } = require('./APIs/Login');
+
 
 dbConnect();
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+app.use(cors());
+
+app.post("/Login",Login);
+
+const PORT = 8000;
+app.listen(PORT);
